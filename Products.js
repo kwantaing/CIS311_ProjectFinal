af59f8dbefcd371d9d0b8deeccfe7a18f5cc9847
Quentin Ng <qng1997@gmail.com>
2019-05-24 17:42:12 -0700

    first commit

---
+++ Products.js
@@ -0,0 +1,17 @@
+var images = ["1.png","2.png","3.png","4.png","5.png","6.png"];
+var captions = ["Just Cause 4","Counter-Strike:Global Offensive","Ride 3","Left For Dead 2","Rocket League","PUBG"];
+var descriptions = ["Welcome to Solís, a huge South American world home of conflict, oppression and extreme weather conditions. \n\nJust Cause 4 sees rogue hero Rico Rodriguez land in Solis to hunt down the truth about his past, at any costs. Strap into your wingsuit, equip your fully customizable grappling hook, and get ready to bring the thunder!",
+                    "Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. \n\nCS: GO features new maps, characters, weapons, and game modes, and delivers updated versions of the classic CS content (de_dust2, etc.).",
+                    "Feel the adrenaline and experience the most complete racing ever with RIDE 3! \n\nImmerse yourself in a modern 3D environment where you will live side by side with your bike, modifying it mechanically and aesthetically thanks to the new Livery Editor, which will let your dreams run wild. Before starting, don't forget to customise your rider with the right outfit.",
+                    "Set in the zombie apocalypse, Left 4 Dead 2 (L4D2) is the highly anticipated sequel to the award-winning Left 4 Dead, the #1 co-op game of 2008.\n\nThis co-operative action horror FPS takes you and your friends through the cities, swamps and cemeteries of the Deep South, from Savannah to New Orleans across five expansive campaigns. \n\nYou'll play as one of four new survivors armed with a wide and devastating array of classic and upgraded weapons. In addition to firearms, you'll also get a chance to take out some aggression on infected with a variety of carnage-creating melee weapons, from chainsaws to axes and even the deadly frying pan.",
+                    "Soccer meets driving once again in the long-awaited, physics-based sequel to the beloved arena classic, Supersonic Acrobatic Rocket-Powered Battle-Cars! \n\nA futuristic Sports-Action game, Rocket League®, equips players with booster-rigged vehicles that can be crashed into balls for incredible goals or epic saves across multiple, highly-detailed arenas. Using an advanced physics system to simulate realistic interactions, Rocket League® relies on mass and momentum to give players a complete sense of intuitive control in this unbelievable, high-octane re-imagining of association football.",
+                    "PLAYERUNKNOWN'S BATTLEGROUNDS is a battle royale shooter that pits 100 players against each other in a struggle for survival. Gather supplies and outwit your opponents to become the last person standing. \n\nPLAYERUNKNOWN, aka Brendan Greene, is a pioneer of the battle royale genre and the creator of the battle royale game modes in the ARMA series and H1Z1: King of the Kill. \n\nAt PUBG Corp., Greene is working with a veteran team of developers to make PUBG into the world's premiere battle royale experience."];
+var price = [59.99,14.99,49.99,9.99,19.99,29.99];
+var url;
+console.log(images);
+for(var i=0; i < images.length; i++){
+  images[i];
+  captions[i];
+  descriptions[i];
+  price[i];
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+This version attempts to address the issue of saving current user information as an object throughout the website
+
+
+PROGRESS: can recognize which user is logged in, 
